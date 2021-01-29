b8515a84c43f3dc8af8b174a4a5aa21088434373
Mikhail Petrov <pmgheap.sbt@gmail.com>
2021-01-29 10:24:36 +0300

    wip

--- modules/spring-cache-ext/src/main/java/org/apache/ignite/cache/spring/SpringCache.java
+++ modules/spring-cache-ext/src/main/java/org/apache/ignite/cache/spring/SpringCache.java
@@ -17,23 +17,13 @@
 
 package org.apache.ignite.cache.spring;
 
-import java.io.Serializable;
 import java.util.concurrent.Callable;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.IgniteLock;
-import org.springframework.cache.Cache;
-import org.springframework.cache.support.SimpleValueWrapper;
+import org.apache.ignite.springdata.proxy.IgniteCacheProxyImpl;
 
-/**
- * Spring cache implementation.
- */
-class SpringCache implements Cache {
-    /** */
-    private static final Object NULL = new NullValue();
-
-    /** */
-    private final IgniteCache<Object, Object> cache;
-
+/** Represents {@link AbstractSpringCache} implementation that uses {@link IgniteCache} to perform cache operations. */
+class SpringCache extends AbstractSpringCache {
     /** */
     private final SpringCacheManager mgr;
 
@@ -42,46 +32,12 @@ class SpringCache implements Cache {
      * @param mgr Manager
      */
     SpringCache(IgniteCache<Object, Object> cache, SpringCacheManager mgr) {
-        assert cache != null;
+        super(new IgniteCacheProxyImpl<>(cache));
 
-        this.cache = cache;
         this.mgr = mgr;
     }
 
     /** {@inheritDoc} */
-    @Override public String getName() {
-        return cache.getName();
-    }
-
-    /** {@inheritDoc} */
-    @Override public Object getNativeCache() {
-        return cache;
-    }
-
-    /** {@inheritDoc} */
-    @Override public ValueWrapper get(Object key) {
-        Object val = cache.get(key);
-
-        return val != null ? fromValue(val) : null;
-    }
-
-    /** {@inheritDoc} */
-    @SuppressWarnings("unchecked")
-    @Override public <T> T get(Object key, Class<T> type) {
-        Object val = cache.get(key);
-
-        if (NULL.equals(val))
-            val = null;
-
-        if (val != null && type != null && !type.isInstance(val))
-            throw new IllegalStateException("Cached value is not of required type [cacheName=" + cache.getName() +
-                ", key=" + key + ", val=" + val + ", requiredType=" + type + ']');
-
-        return (T)val;
-    }
-
-    /** {@inheritDoc} */
-    @SuppressWarnings("unchecked")
     @Override public <T> T get(final Object key, final Callable<T> valLdr) {
         Object val = cache.get(key);
 
@@ -112,61 +68,5 @@ class SpringCache implements Cache {
         }
 
         return (T)unwrapNull(val);
-    }
-
-    /** {@inheritDoc} */
-    @Override public void put(Object key, Object val) {
-        if (val == null)
-            cache.withSkipStore().put(key, NULL);
-        else
-            cache.put(key, val);
-    }
-
-    /** {@inheritDoc} */
-    @Override public ValueWrapper putIfAbsent(Object key, Object val) {
-        Object old;
-
-        if (val == null)
-            old = cache.withSkipStore().getAndPutIfAbsent(key, NULL);
-        else
-            old = cache.getAndPutIfAbsent(key, val);
-
-        return old != null ? fromValue(old) : null;
-    }
-
-    /** {@inheritDoc} */
-    @Override public void evict(Object key) {
-        cache.remove(key);
-    }
-
-    /** {@inheritDoc} */
-    @Override public void clear() {
-        cache.removeAll();
-    }
-
-    /**
-     * @param val Cache value.
-     * @return Wrapped value.
-     */
-    private static ValueWrapper fromValue(Object val) {
-        assert val != null;
-
-        return new SimpleValueWrapper(unwrapNull(val));
-    }
-
-    private static Object unwrapNull(Object val) {
-        return NULL.equals(val) ? null : val;
-    }
-
-    private <T> Object wrapNull(T val) {
-        return val == null ? NULL : val;
-    }
-
-    /** */
-    private static class NullValue implements Serializable {
-        /** {@inheritDoc} */
-        @Override public boolean equals(Object o) {
-            return this == o || (o != null && getClass() == o.getClass());
-        }
     }
 }
--- modules/spring-cache-ext/src/test/java/org/apache/ignite/cache/spring/GridSpringCacheManagerAbstractTest.java
+++ modules/spring-cache-ext/src/test/java/org/apache/ignite/cache/spring/GridSpringCacheManagerAbstractTest.java
@@ -41,7 +41,7 @@ public abstract class GridSpringCacheManagerAbstractTest extends GridCommonAbstr
      */
     static {
         try {
-            NULL = U.field(SpringCache.class, "NULL");
+            NULL = U.field(AbstractSpringCache.class, "NULL");
         }
         catch (IgniteCheckedException e) {
             throw new RuntimeException(e);
